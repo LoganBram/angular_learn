608c6ac5bea6d508af94df45b8c403c4b0028972
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-11 14:14:18 -0700

    added todo and all tasks functionality

--- helper/src/app/list/list.component.ts
+++ helper/src/app/list/list.component.ts
@@ -11,6 +11,18 @@ import {tasks, Task} from '../tasks';
 export class ListComponent {
 
     tasks = [...tasks]
+    filter: "all" | "active" | "done" = "all";
+
+    get obtainTasks(){
+      if (this.filter === "all"){
+        return this.tasks
+      }
+      return this.tasks.filter((task)=> !task.done
+      )
+    }
+
+
+    
 
     onDelete(task: Task){
       this.tasks.splice(this.tasks.indexOf(task), 1)
