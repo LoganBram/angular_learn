5330b6aba264f801a7c410ab6f44e9080a44c2af
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-11 11:23:33 -0700

    http module calibration and assets folder added

--- prodlist2/src/app/cart.service.ts
+++ prodlist2/src/app/cart.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import {HttpClient } from '@angular/common/http'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = [];
+
+
+  constructor(private http: HttpClient){}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,5 +25,4 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
 }
