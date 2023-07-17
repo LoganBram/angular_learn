70cd44ae8da63ee0fec0f41576587ae35ead2527
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-17 10:23:52 -0700

    reformatting adn dcss

--- weather_solo/src/app/currweather/currweather.component.ts
+++ weather_solo/src/app/currweather/currweather.component.ts
@@ -19,6 +19,7 @@ export class CurrweatherComponent implements OnInit {
   @Input() city!: any;
   locationObj: any;
   forecastObj: any;
+  icon: any;
   date: any = new Date();
   pipe = new DatePipe('en-US');
 
@@ -40,14 +41,18 @@ export class CurrweatherComponent implements OnInit {
       // get weather of current location
       this.getWeather(this.locationObj).subscribe((weatherResponse) => {
         this.forecastObj = weatherResponse;
+        //set icon value 
+        this.icon  = "https://openweathermap.org/img/wn/" + this.forecastObj.weather[0].icon + ".png";
         console.log(this.forecastObj);
         console.log(this.locationObj)
-        // Do something with the weather data here
       });
     });
 
     //get todays date and change format
     this.date = this.pipe.transform(this.date, 'yyyy-MM-dd');
+
+    
+
 
   }
 }
